f6aeca314a967275c0755088a624d01f952d30f4
Dezhi <dzhong1989@gmail.com>
2014-09-09 22:04:46 -0700

    finished iterative clf script

--- type_skf_itr.py
+++ type_skf_itr.py
@@ -10,14 +10,15 @@ from sklearn import tree
 from sklearn.preprocessing import normalize
 import numpy as np
 import math
+import random
 import pylab as pl
 
 input1 = np.genfromtxt('rice_45min', delimiter=',')
-data2 = input1[:,0:-1]
-label2 = input1[:,-1]
+data1 = input1[:,0:-1]
+label1 = input1[:,-1]
 input2 = np.genfromtxt('sdh_45min', delimiter=',')
-data1 = input2[:,0:-1]
-label1 = input2[:,-1]
+data2 = input2[:,0:-1]
+label2 = input2[:,-1]
 
 '''
 loo = LeaveOneOut(len(data))
@@ -42,78 +43,67 @@ for train_idx, test_idx in skf:
     aka, use 1 fold to train, k-1 folds to test
     so the indexing is inversed
     '''
-    train_data = data1[test_idx]
-    train_label = label1[test_idx]
-    test_data = data1[train_idx]
-    test_label = label1[train_idx]
-    #test_data = data2
-    #test_label = label2
-    clf.fit(train_data, train_label)
-    preds = clf.predict(test_data)
-    acc = accuracy_score(test_label, preds)
-    acc_sum.append(acc)
-    #print acc
+    for ctr in range(20):
+        print len(test_idx), 'training exs'
+        print len(train_idx), 'testing exs'
+        train_data = data1[test_idx]
+        train_label = label1[test_idx]
+        test_data = data1[train_idx]
+        test_label = label1[train_idx]
+        clf.fit(train_data, train_label)
+        preds = clf.predict(test_data)
 
-    cm = CM(test_label,preds)
-    cm = normalize(cm.astype(np.float), axis=1, norm='l1')
-    k=0
-    while k<6:
-        indi_acc[k].append(cm[k,k])
-        k += 1
+        #overall acc
+        acc = accuracy_score(test_label, preds)
+        acc_sum.append(acc)
+        #print acc
 
-    #log = open('log_r_s','w')
-    label_pr = clf.predict_proba(test_data)
-    correct = []
-    wrong = []
-    for h,i,j,pr in zip(train_idx,test_label,preds,label_pr):
-        entropy = np.sum(-p*math.log(p,6) for p in pr if p!=0)
-        if i==j:
-            correct.append([h,i,j,entropy])
-        else:
-            wrong.append([h,i,j,entropy])
-        #log.write('[%d]-%d:%d'%(k,i,j))
-        #log.write('-%s'%pr)
-        #log.write('-%.3f\n'%entropy)
-    #log.close()
+        #acc by type
+        cm = CM(test_label,preds)
+        cm = normalize(cm.astype(np.float), axis=1, norm='l1')
+        k=0
+        while k<6:
+            indi_acc[k].append(cm[k,k])
+            k += 1
 
-    wrong = sorted(wrong,key=lambda x: x[3])
-    ele = wrong[0][0]
-    test_idx = np.append(test_idx, ele)
-    trian_idx = train_idx[train_idx!=ele]
+        #compute entropy for each instance and rank
+        label_pr = clf.predict_proba(test_data)
+        correct = []
+        wrong = []
+        for h,i,j,pr in zip(train_idx,test_label,preds,label_pr):
+            entropy = np.sum(-p*math.log(p,6) for p in pr if p!=0)
+            if i==j:
+                correct.append([h,i,j,entropy])
+            else:
+                wrong.append([h,i,j,entropy])
+        '''
+        #sort and pick the 1st one with largest H
+        wrong = sorted(wrong, key=lambda x: x[3], reverse=True)
+        idx = 0
 
+        #randomly pick one
+        idx = random.randint(0,len(wrong)-1)
+        '''
 
+        #pick the one with H most close to 0.5
+        for i in wrong:
+            i[-1] = abs(i[-1]-0.5)
+        wrong = sorted(wrong, key=lambda x: x[3])
+        idx = 0
+
+        elmt = wrong[idx][0]
+        print 'ex H:', wrong[idx][-1]
+        print '================='
+        #remove the item on the top of the ranked wrong list from the training set
+        #add it to test set
+        test_idx = np.append(test_idx, elmt)
+        train_idx = train_idx[train_idx!=elmt]
+
+    break
 indi_ave_acc = [np.mean(i) for i in indi_acc]
 #indi_ave_acc_std = [np.std(i) for i in indi_acc]
-print 'ave acc/type:', repr(indi_ave_acc)
+#print 'ave acc/type:', repr(indi_ave_acc)
 #print 'acc std/type:', indi_ave_acc_std
-print 'ave acc:', np.mean(acc_sum)
+print 'overall acc:', repr(acc_sum)
+#print 'ave cc :', np.mean(acc_sum)
 #print 'acc std:', np.std(acc_sum)
-
-'''
-%plot confusion matrix
-cm = CM(test_label,preds)
-#print cm
-cm = normalize(cm.astype(np.float), axis=1, norm='l1')
-#print cm
-#cm /= cm.astype(np.float).sum(axis=1)
-fig = pl.figure()
-ax = fig.add_subplot(111)
-cax = ax.matshow(cm)
-fig.colorbar(cax)
-
-for x in xrange(len(cm)):
-    for y in xrange(len(cm)):
-        ax.annotate(str("%.3f"%cm[x][y]), xy=(y,x),
-                    horizontalalignment='center',
-                    verticalalignment='center')
-
-
-cls = ['co2','humidity','rmt','stpt','flow','other_t']
-pl.xticks(range(len(cm)),cls)
-pl.yticks(range(len(cm)),cls)
-pl.title('Confusion matrix')
-#pl.colorbar()
-pl.ylabel('True label')
-pl.xlabel('Predicted label')
-pl.show()
-'''
