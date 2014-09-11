5bcfcd4d72648e52b427624996b368461db79865
hdz <dzhong1989@gmail.com>
2014-09-11 12:45:18 -0400

    itr starts from zero percent

--- type_skf_itr.py
+++ type_skf_itr.py
@@ -14,10 +14,10 @@ import random
 import pylab as pl
 
 input1 = np.genfromtxt('rice_45min', delimiter=',')
-data1 = input1[:,0:-1]
+data1 = input1[:,[0,1,2,3,5,6,7]]
 label1 = input1[:,-1]
 input2 = np.genfromtxt('sdh_45min', delimiter=',')
-data2 = input2[:,0:-1]
+data2 = input2[:,[0,1,2,3,5,6,7]]
 label2 = input2[:,-1]
 
 '''
@@ -26,31 +26,34 @@ for train_idx, test_idx in loo:
     pass
 '''
 
-ctr = 0
-preds = []
-fold = 10
-skf = StratifiedKFold(label1, n_folds=fold)
-acc_sum = [[] for i in range(fold)]
+run = 10
+iteration = 20
+init = 20
+#skf = StratifiedKFold(label1, n_folds=fold)
+acc_sum = [[] for i in range(run)]
 indi_acc =[[] for i in range(6)]
 #clf = ETC(n_estimators=10, criterion='entropy')
 clf = RFC(n_estimators=50, criterion='entropy')
 #clf = DT(criterion='entropy', random_state=0)
 #clf = Ada(n_estimators=100)
 #clf = SVC(kernel='linear')
+preds = []
+
 itr = 0
-for train_idx, test_idx in skf:
-    '''
-    because we want to do inverse k-fold XV
-    aka, use 1 fold to train, k-1 folds to test
-    so the indice is switched
-    '''
-    for ctr in range(20):
-        #print len(test_idx), 'training exs'
-        #print len(train_idx), 'testing exs'
-        train_data = data1[test_idx]
-        train_label = label1[test_idx]
-        test_data = data1[train_idx]
-        test_label = label1[train_idx]
+for itr in range(run):
+    train_idx = []
+    test_idx = range(len(data1))
+    for i in range(init):
+        tmp = random.randint(0,len(test_idx)-1)
+        idx = test_idx[tmp]
+        train_idx.append(idx)
+        test_idx.remove(idx)
+
+    for ctr in range(iteration):
+        train_data = data1[train_idx]
+        train_label = label1[train_idx]
+        test_data = data1[test_idx]
+        test_label = label1[test_idx]
         clf.fit(train_data, train_label)
         preds = clf.predict(test_data)
 
@@ -73,18 +76,19 @@ for train_idx, test_idx in skf:
         label_pr = clf.predict_proba(test_data)
         correct = []
         wrong = []
-        for h,i,j,pr in zip(train_idx,test_label,preds,label_pr):
+        for h,i,j,pr in zip(test_idx,test_label,preds,label_pr):
             entropy = np.sum(-p*math.log(p,6) for p in pr if p!=0)
             if i==j:
                 correct.append([h,i,j,entropy])
             else:
                 wrong.append([h,i,j,entropy])
+
         '''
         #sort and pick the 1st one with largest H
         wrong = sorted(wrong, key=lambda x: x[3], reverse=True)
         idx = 0
 
-        '''
+
         #randomly pick one
         idx = random.randint(0,len(wrong)-1)
 
@@ -94,20 +98,19 @@ for train_idx, test_idx in skf:
             i[-1] = abs(i[-1]-0.5)
         wrong = sorted(wrong, key=lambda x: x[3])
         idx = 0
-        '''
+
 
         elmt = wrong[idx][0]
         #print 'ex H:', wrong[idx][-1]
-        #print '================='
         #remove the item on the top of the ranked wrong list from the training set
         #add it to test set
-        test_idx = np.append(test_idx, elmt)
-        train_idx = train_idx[train_idx!=elmt]
+        train_idx.append(elmt)
+        test_idx.remove(elmt)
 
     itr+=1
-    #break
+
 ave_acc = []
-for i in range(20):
+for i in range(iteration):
     l = [acc[i] for acc in acc_sum]
     ave_acc.append(np.mean(l))
 
