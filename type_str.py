b2206b3ca423d52cfc3314b56349b9c92f254765
hdz <dzhong1989@gmail.com>
2014-09-25 15:29:33 -0400

    minor changes

--- type_str.py
+++ type_str.py
@@ -24,14 +24,7 @@ input1 = [i.strip().split('\\')[-1][:-4] for i in open('rice_pt_name').readlines
 input2 = np.genfromtxt('rice_45min', delimiter=',')
 label = input2[:,-1]
 
-'''
-loo = LeaveOneOut(len(data))
-for train_idx, test_idx in loo:
-    pass
-'''
-
-ctr = 0
-fold = 5
+fold = 2
 skf = StratifiedKFold(label, n_folds=fold)
 acc_sum = []
 indi_acc =[[] for i in range(6)]
@@ -42,8 +35,9 @@ indi_acc =[[] for i in range(6)]
 clf = SVC(kernel='linear')
 #clf = GNB()
 
-vc = CV(token_pattern='[a-z]{2,}')
-#vc_ = CV(analyzer='char_wb', ngram_range=(2,4#), min_df=1, token_pattern='[a-z]{2,}')
+#vc = CV(token_pattern='[a-z]{2,}')
+#vc = TV(token_pattern='[a-z]{2,}')
+vc = CV(analyzer='char_wb', ngram_range=(2,4), min_df=1, token_pattern='[a-z]{2,}')
 data = vc.fit_transform(input1).toarray()
 for train_idx, test_idx in skf:
     '''
