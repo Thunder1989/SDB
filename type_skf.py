4a37f324d7f4cd76ff74394bcf23bebd8a68db66
Dezhi <dzhong1989@gmail.com>
2014-09-29 21:47:16 -0400

    further divided other_t and modified the source data file

--- type_skf.py
+++ type_skf.py
@@ -12,7 +12,7 @@ import numpy as np
 import math
 import pylab as pl
 
-input1 = np.genfromtxt('rice_45min', delimiter=',')
+input1 = np.genfromtxt('rice_45min_', delimiter=',')
 data1 = input1[:,[0,1,2,3,5,6,7]]
 label1 = input1[:,-1]
 input2 = np.genfromtxt('sdh_45min', delimiter=',')
@@ -26,16 +26,16 @@ for train_idx, test_idx in loo:
 '''
 
 ctr = 0
-preds = []
 fold = 2
+clx = 17
 skf = StratifiedKFold(label1, n_folds=fold)
 acc_sum = []
-indi_acc =[[] for i in range(6)]
+indi_acc =[[] for i in range(clx)]
 #clf = ETC(n_estimators=10, criterion='entropy')
-#clf = RFC(n_estimators=50, criterion='entropy')
+clf = RFC(n_estimators=50, criterion='entropy')
 #clf = DT(criterion='entropy', random_state=0)
 #clf = Ada(n_estimators=100)
-clf = SVC(kernel='linear')
+#clf = SVC(kernel='linear')
 loop = 0
 run = 10
 importance = np.zeros(data1.shape[1])
@@ -53,7 +53,6 @@ while loop<run/fold:
         #test_data = data2
         #test_label = label2
         clf.fit(train_data, train_label)
-        #out = tree.export_graphviz(clf, out_file='tree.dot')
         preds = clf.predict(test_data)
         acc = accuracy_score(test_label, preds)
         acc_sum.append(acc)
@@ -62,7 +61,7 @@ while loop<run/fold:
         cm_ = CM(test_label,preds)
         cm = normalize(cm_.astype(np.float), axis=1, norm='l1')
         k=0
-        while k<6:
+        while k<clx:
             indi_acc[k].append(cm[k,k])
             k += 1
 
@@ -121,10 +120,9 @@ log.close()
 print 'ave acc:', np.mean(acc_sum)
 #print 'std:', np.std(acc_sum)
 
-'''
-cm = CM(test_label,preds)
+cm_ = CM(test_label,preds)
 #print cm
-cm = normalize(cm.astype(np.float), axis=1, norm='l1')
+cm = normalize(cm_.astype(np.float), axis=1, norm='l1')
 #print cm
 #cm /= cm.astype(np.float).sum(axis=1)
 fig = pl.figure()
@@ -134,7 +132,7 @@ fig.colorbar(cax)
 
 for x in xrange(len(cm)):
     for y in xrange(len(cm)):
-        ax.annotate(str("%.3f"%cm[x][y]), xy=(y,x),
+        ax.annotate(str("%.3f(%d)"%(cm[x][y],cm_[x][y])), xy=(y,x),
                     horizontalalignment='center',
                     verticalalignment='center')
 
@@ -142,9 +140,8 @@ for x in xrange(len(cm)):
 cls = ['co2','humidity','rmt','stpt','flow','water T','air T']
 pl.xticks(range(len(cm)),cls)
 pl.yticks(range(len(cm)),cls)
-pl.title('Confusion matrix')
+pl.title('Confusion matrix (%.3f)'%acc)
 #pl.colorbar()
 pl.ylabel('True label')
 pl.xlabel('Predicted label')
 pl.show()
-'''
