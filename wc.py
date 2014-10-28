7489aa082cecc349272596c3589167ad227d3899
hdz <dzhong1989@gmail.com>
2014-10-28 11:01:38 -0400

    fixed bugs in wc

--- wc.py
+++ wc.py
@@ -5,6 +5,8 @@ import operator
 
 input1 = [i.strip().split('\\')[-1][:-4] for i in open('rice_pt_name').readlines()]
 input2 = np.genfromtxt('rice_45min', delimiter=',')
+#input1 = [i.strip().split('\\')[-1][:-4] for i in open('sdh_pt_name').readlines()]
+#input2 = np.genfromtxt('sdh_45min', delimiter=',')
 label = input2[:,-1]
 vc = CV(token_pattern='[a-z]{2,}')
 #vc = CV(analyzer='char_wb', ngram_range=(2,4), min_df=1, token_pattern='[a-z]{2,}')
@@ -13,6 +15,12 @@ for i,j in zip(label,input1):
     data[i].append(j)
 for i,j in data.items():
     data = vc.fit_transform(j).toarray()
-    d = vc.vocabulary_
-    d = sorted(d.items(), key=operator.itemgetter(1))
-    print i,'===', d
+    f = data.sum(axis=0)
+    t = vc.get_feature_names()
+    count = zip(t, f)
+    count = sorted(count, key=operator.itemgetter(1))
+    print '=== %s (%s) ==='%(i, data.shape[0])
+    for c in count:
+        print c
+    #d = sorted(d.items(), key=operator.itemgetter(1))
+    #print i,'===', d
